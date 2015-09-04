9ccd7ad13ef69abc246cc3812e8da9568af6e8f3
Thomas Levine <_@thomaslevine.com>
2015-09-04 08:03:33 -0400

    setup and readme

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(name='date_range',
+    maintainer='Thomas Levine',
+    maintainer_email='_@thomaslevine.com',
+    description='Like the builtin range function, but for datetime.date objects',
+    url='https://github.com/tlevine/date_range.git',
+    classifiers=[
+        'Intended Audience :: Developers',
+    ],
+    py_modules=['date_range'],
+    install_requires = [],
+    tests_require = ['pytest'],
+    version='0.0.1',
+    license='LGPL'
+)
