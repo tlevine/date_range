b51505d90ee232b07d90f0686a0cb1d67e664df2
Thomas Levine <_@thomaslevine.com>
2015-09-04 08:58:42 -0400

    more description in setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,17 @@ from distutils.core import setup
 setup(name='date_range',
     maintainer='Thomas Levine',
     maintainer_email='_@thomaslevine.com',
-    description='Like the builtin range function, but for datetime.date objects',
+    description='Produce a sequence of dates. It\'s like the builtin range function but for datetime.date objects',
     url='https://github.com/tlevine/date_range.git',
     classifiers=[
         'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
     ],
     py_modules=['date_range'],
-    install_requires = [],
     version='0.0.1',
     license='LGPL'
 )
