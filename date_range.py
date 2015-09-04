fe66c8ab88be4d4cfb3fb1450b01cc3b5e7f10f2
Thomas Levine <_@thomaslevine.com>
2015-09-04 08:10:12 -0400

    handle more range queries

--- date_range.py
+++ date_range.py
@@ -1,4 +1,14 @@
 import datetime
 
-def date_range(left, right):
-    return map(datetime.date.fromordinal, range(left.toordinal(), right.toordinal()))
+def date_range(*args):
+    '''
+    range(stop) -> iterable of datetime.date objects
+    range(start, stop[, step]) -> iterable of datetime.date objects
+    '''
+    if len(args) == 3:
+        start, stop, step = args
+        parsed_args = (start.toordinal(), stop.toordinal(), step)
+    else:
+        parsed_args = tuple(x.toordinal() for x in args)
+
+    return map(datetime.date.fromordinal, range(*parsed_args))
