330579241c7af26e88ab91eb11e5cbdd09a74e70
Thomas Levine <_@thomaslevine.com>
2015-09-04 08:16:59 -0400

    doctetss

--- date_range.py
+++ date_range.py
@@ -4,6 +4,16 @@ def date_range(*args):
     '''
     range(stop) -> iterable of datetime.date objects
     range(start, stop[, step]) -> iterable of datetime.date objects
+
+    >>> list(date_range(datetime.date(2015, 1, 1), datetime.date(2015, 1, 3)))
+    [datetime.date(2015, 1, 1), datetime.date(2015, 1, 2)]
+
+    >>> list(datetime.date(2015, 1, 1), datetime.date(2015, 1, 6), 2)
+    [datetime.date(2015, 1, 1), datetime.date(2015, 1, 3), datetime.date(2015, 1, 5)]
+
+#   >>> list(datetime.date(2015, 1, 1), datetime.date(2015, 1, 6), datetime.timedelta(2))
+#   [datetime.date(2015, 1, 1), datetime.date(2015, 1, 3), datetime.date(2015, 1, 5)]
+
     '''
     if len(args) == 3:
         start, stop, step = args
