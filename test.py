e043c002e4da0e7f50c16192fe97d7830b44aa85
Thomas Levine <_@thomaslevine.com>
2015-09-04 08:06:49 -0400

    more tests

--- test.py
+++ test.py
@@ -5,10 +5,20 @@ import pytest
 from date_range import date_range
 
 testcases = [
-    (datetime.date(2015, 1, 1), datetime.date(2015, 1, 3),
-        [datetime.date(2015, 1, 1), datetime.date(2015, 1, 2)]),
+    (
+        (datetime.date(2015, 1, 1), datetime.date(2015, 1, 3)),
+        [datetime.date(2015, 1, 1), datetime.date(2015, 1, 2)]
+    ),
+    (
+        (datetime.date(2015, 1, 1), datetime.date(2015, 1, 6), 2),
+        [
+            datetime.date(2015, 1, 1),
+            datetime.date(2015, 1, 3),
+            datetime.date(2015, 1, 5),
+        ]
+    ),
 ]
 
-@pytest.mark.parametrize('left, right, output', testcases)
-def test_date_range(left, right, output):
-    assert list(date_range(left, right)) == output
+@pytest.mark.parametrize('args, output', testcases)
+def test_date_range(args, output):
+    assert list(date_range(*args)) == output
