c334b1a5e0100e4640f14c7a1a91110efb4abeb1
Thomas Levine <_@thomaslevine.com>
2015-09-04 08:46:45 -0400

    switch tests to doctests

--- test.py
+++ test.py
@@ -1,24 +1,5 @@
-import datetime
+import doctest
 
-import pytest
+import date_range
 
-from date_range import date_range
-
-testcases = [
-    (
-        (datetime.date(2015, 1, 1), datetime.date(2015, 1, 3)),
-        [datetime.date(2015, 1, 1), datetime.date(2015, 1, 2)]
-    ),
-    (
-        (datetime.date(2015, 1, 1), datetime.date(2015, 1, 6), 2),
-        [
-            datetime.date(2015, 1, 1),
-            datetime.date(2015, 1, 3),
-            datetime.date(2015, 1, 5),
-        ]
-    ),
-]
-
-@pytest.mark.parametrize('args, output', testcases)
-def test_date_range(args, output):
-    assert list(date_range(*args)) == output
+doctest.testmod(date_range)
